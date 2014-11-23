f0b9237eb56ce0acc43f5cc06980ea2c3b0d45d3
bpineda <bernardo.pineda@gmail.com>
2014-11-23 16:41:02 -0600

    Indentation on nav controller

--- app/js/controllers/navctrl.js
+++ app/js/controllers/navctrl.js
@@ -14,19 +14,25 @@ define([], function() {
                     $scope.current_url = $route.current.originalPath;
                     $scope.isLoggedIn = true;
 
-                    $scope.$on("$locationChangeStart", function(event, next, current) {
-                    var current_page_array = next.split('/');
-                    var current_page = current_page_array.pop();
-                    $scope.current_url = '/' + current_page;
-                    //console.log(current_page);
-                    if(current_page == 'exit')
-                    {
+                    $scope.$on("$locationChangeStart",  function(    event,
+                                                                    next,
+                                                                    current)
+                                                        {
 
-                        $rootScope.isLoggedIn = false;
+                                                            var current_page_array = next.split('/');
+                                                            var current_page = current_page_array.pop();
+                                                            $scope.current_url = '/' + current_page;
+                                                            //console.log(current_page);
+                                                            if(current_page == 'exit')
+                                                            {
 
-                    }
-                  });
+                                                                $rootScope.isLoggedIn = false;
+
+                                                            }
+
+                                                        });
                     $scope.$apply();
 
                 }];
+
 });
