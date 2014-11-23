bd942b122578d03139f3d56032b97d7025028f75
bpineda <bernardo.pineda@gmail.com>
2014-11-23 17:03:18 -0600

    Created factories file to be able to call out services

---
+++ app/js/factories.js
@@ -0,0 +1,29 @@
+'use strict';
+
+define(['angular'], function (angular)
+{
+
+    /* Factories */
+
+    return angular.module('myApp.factories', [])
+
+        //Factory Examples:
+        .factory(   'ExampleMethod1',
+                    function ( $resource )
+                    {
+
+                        return $resource( 'http://path-to-the-service/' );
+
+                    })
+        .factory(   'ExampleMethod2',
+                    function (  $resource,
+                                VAR1,
+                                VAR2)
+                    {
+
+                        return $resource( VAR1 + '/method/?param1=0&param2=' + VAR2);
+
+                    })
+
+
+});
