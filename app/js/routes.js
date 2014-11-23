ff3dc0ecda1e8d9a0f4999b48fe82ca0a461ec01
bpineda <bernardo.pineda@gmail.com>
2014-11-23 16:45:21 -0600

    Defined new route to json service

--- app/js/routes.js
+++ app/js/routes.js
@@ -2,16 +2,27 @@
 
 define(['angular', 'app'], function(angular, app) {
 
-	return app.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/view1', {
-			templateUrl: 'app/partials/partial1.html',
-			controller: 'MyCtrl1'
-		});
-		$routeProvider.when('/view2', {
-			templateUrl: 'app/partials/partial2.html',
-			controller: 'MyCtrl2'
-		});
-		$routeProvider.otherwise({redirectTo: '/view1'});
-	}]);
+	return app.config([	'$routeProvider',
+	                  	function($routeProvider)
+											{
+
+												$routeProvider.when('/view1', {
+													templateUrl: 'app/partials/partial1.html',
+													controller: 'MyCtrl1'
+												});
+
+												$routeProvider.when('/view2', {
+													templateUrl: 'app/partials/partial2.html',
+													controller: 'MyCtrl2'
+												});
+
+												$routeProvider.when('/json_service', {
+													templateUrl: 'app/partials/json_service.html',
+													controller: 'JsonServiceCtrl'
+												});
+
+												$routeProvider.otherwise({redirectTo: '/view1'});
+
+											}]);
 
 });
