939f5588436261ed20bb828e8029644b3a6e1dc1
bpineda <bernardo.pineda@gmail.com>
2014-11-23 17:58:52 -0600

    Added constants to refer to the URL of the JSON service

--- app/js/app.js
+++ app/js/app.js
@@ -20,12 +20,16 @@ define([
 
 				// Declare app level module which depends on filters, and services
 
-				return angular.module('myApp', [	'ngRoute',
+				return angular
+								.module('myApp', [	'ngRoute',
 																					'myApp.filters',
 																					'myApp.services',
 																					'myApp.directives',
 																					'myApp.controllers'
-																				]);
+																				])
+								.constant(	'API_HOST',
+								          	'http://www.telize.com')
+				;
 
 			}
 );
