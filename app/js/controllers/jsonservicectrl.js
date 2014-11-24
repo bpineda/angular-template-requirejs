a93e988a51ff56968acc2ff7ca07c4c28bd34e0c
bpineda <bernardo.pineda@gmail.com>
2014-11-23 20:05:23 -0600

    Called location service through a resource

--- app/js/controllers/jsonservicectrl.js
+++ app/js/controllers/jsonservicectrl.js
@@ -7,8 +7,26 @@ define([], function() {
                       GetLocation)
             {
 
-              console.log(123);
+              $scope.callJSONService =  function()
+                                        {
+
+                                          //
+                                          GetLocation.get(  function( response )
+                                                            {
+
+                                                              $scope.JSONLocationData = response;
+
+                                                            },
+                                                            function( error )
+                                                            {
+
+                                                              alert('error');
+
+                                                            });
+
+                                        }
               $scope.welcomeMessage = 'JSON Service Controller';
+              $scope.callJSONService();
 
               $scope.$apply();
 
